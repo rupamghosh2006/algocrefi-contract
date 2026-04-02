fbfd57f98b4b453f362ae6515fcc7ed12a84fe31
Rupam Ghosh <rupamgh2006@gmail.com>
2026-04-02 17:29:55 +0000

    deployed 1st contract on test net

--- projects/algocrefi-contract/smart_contracts/algocrefi_pool/contract.py
+++ projects/algocrefi-contract/smart_contracts/algocrefi_pool/contract.py
@@ -1,8 +1,16 @@
-from algopy import ARC4Contract, String
+from algopy import ARC4Contract, UInt64
 from algopy.arc4 import abimethod
 
 
 class AlgocrefiPool(ARC4Contract):
+
+    def __init__(self) -> None:
+        self.pool = UInt64(0)
+
     @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    def deposit(self, amount: UInt64) -> None:
+        self.pool += amount
+
+    @abimethod()
+    def get_pool(self) -> UInt64:
+        return self.pool
--- projects/algocrefi-contract/smart_contracts/algocrefi_pool/deploy_config.py
+++ projects/algocrefi-contract/smart_contracts/algocrefi_pool/deploy_config.py
@@ -1,14 +1,10 @@
 import logging
-
 import algokit_utils
 
 logger = logging.getLogger(__name__)
 
-
-# define deployment behaviour based on supplied app spec
 def deploy() -> None:
     from smart_contracts.artifacts.algocrefi_pool.algocrefi_pool_client import (
-        HelloArgs,
         AlgocrefiPoolFactory,
     )
 
@@ -16,7 +12,8 @@ def deploy() -> None:
     deployer_ = algorand.account.from_environment("DEPLOYER")
 
     factory = algorand.client.get_typed_app_factory(
-        AlgocrefiPoolFactory, default_sender=deployer_.address
+        AlgocrefiPoolFactory,
+        default_sender=deployer_.address,
     )
 
     app_client, result = factory.deploy(
@@ -24,21 +21,4 @@ def deploy() -> None:
         on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
     )
 
-    if result.operation_performed in [
-        algokit_utils.OperationPerformed.Create,
-        algokit_utils.OperationPerformed.Replace,
-    ]:
-        algorand.send.payment(
-            algokit_utils.PaymentParams(
-                amount=algokit_utils.AlgoAmount(algo=1),
-                sender=deployer_.address,
-                receiver=app_client.app_address,
-            )
-        )
-
-    name = "world"
-    response = app_client.send.hello(args=HelloArgs(name=name))
-    logger.info(
-        f"Called hello on {app_client.app_name} ({app_client.app_id}) "
-        f"with name={name}, received: {response.abi_return}"
-    )
+    logger.info(f"App deployed with ID: {app_client.app_id}")
